64f6c78d2dcb4ab9ec1b53aa68bcd84e33d0cea0
yusranAbd <yusran.abd.arief@gmail.com>
2023-01-10 09:36:49 +0700

    UI: avatar & name

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,17 @@
-// import Image from 'next/image'
+import Image from 'next/image'
+import data from '../data.json'
 
 export default function Home() {
   return(
-    <div className="flex text-">
-
+    <div className="flex flex-col items-center mx-auto w-full mt-16">
+        <Image
+          alt={data.name}
+          src={data.avatar}
+          width={96}
+          height={96}
+          className="rounded-full"
+        />
+        <h1 className='font-mono mt-4 text-xl'>{data.name}</h1>
     </div>
   )
 }
