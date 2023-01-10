904b0e7eb2914f2e40ec4c7bdabfcb31174b558a
yusranAbd <yusran.abd.arief@gmail.com>
2023-01-10 18:34:17 +0700

    move to app directori

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+    return (
+      <>
+        <title>Yusran Abd. Arief</title>
+        <meta name="description" content="Linktree" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </>
+    );
+  }
