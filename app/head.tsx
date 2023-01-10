bc36db623759d73169c65b09fa56d3a6ccb0c145
yusranAbd <yusran.abd.arief@gmail.com>
2023-01-10 18:35:04 +0700

    change title

--- app/head.tsx
+++ app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
     return (
       <>
-        <title>Yusran Abd. Arief</title>
+        <title>Yusran | Linktree</title>
         <meta name="description" content="Linktree" />
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
