3da91f67a50e1c29317f221960edc36e6298482c
yusranAbd <yusran.abd.arief@gmail.com>
2023-01-28 08:42:01 +0700

    change; background

--- app/page.tsx
+++ app/page.tsx
@@ -60,7 +60,7 @@ function LinkCard({ href, title, image, bgshine, disable }: {href: string; title
       href={href}
       target="_blank"
       rel="noopener noreferrer"
-      className={`flex items-center p-1 w-full rounded-md hover:scale-105 transition-all bg-gray-100 mb-3 max-w-sm ${bgshine} ${disable}`}
+      className={`flex items-center p-1 w-full rounded-md hover:scale-105 transition-all shadow-2xl bg-cyan-50 mb-3 max-w-sm ${bgshine} ${disable}`}
     >
       <div className="flex text-center w-full">
         <div className="w-10 h-10">
@@ -114,13 +114,13 @@ export default async function HomePage() {
     <div className="flex items-center flex-col mx-auto w-full justify-center mt-16 px-8">
         <Image
         priority
-        className="rounded-full"
+        className="rounded-full shadow-2xl"
         alt={data.name}
         src={data.avatar}
         width={96}
         height={96}
       />
-      <h1 className=" mt-4 mb-8 text-xl text-black">{data.name}</h1>
+      <h1 className=" mt-4 mb-8 text-xl font-bold text-black">{data.name}</h1>
       {data.links.map((link) => (
         <LinkCard key={link.href} {...link} />
       ))}
